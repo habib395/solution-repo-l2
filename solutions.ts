74a4474f53dcba76b44e9e0b6024c224bf429aca
Md.Habibur Rahman Sujon <md.habiburrahmanjwd@gmail.com>
2025-05-21 08:46:08 +0600

    testing done

---
+++ solutions.ts
@@ -0,0 +1,12 @@
+{
+    //
+    const searchName = (value: string) =>{
+        if(value){
+            console.log('Searching');
+        }else{
+            console.log('There is nothing to search');
+        }
+    }
+    searchName('habib')
+    //
+}
